e1f7adbbbd8c340e6e92085517a1c5d3413017c5
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-05-16 23:27:06 -0300

    feat: 1. Receber nome do cliente

---
+++ 1sem_cp3_minicoffee.py
@@ -0,0 +1,8 @@
+# RM: 571382 - Pedro Henrique Neves
+# RM: 572733 - Akin Alexandre Mendes Martin 
+# RM: 570544 - Maria Eduarda Rocha Benjamin
+
+nome = input('Digite seu nome: ')
+qnt_letras = len(nome)
+primeira_letra = nome[0]
+
