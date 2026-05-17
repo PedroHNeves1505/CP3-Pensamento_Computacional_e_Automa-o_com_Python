3105a44a4a2b333e5e7c5a18d550d30d7fed817f
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-05-16 23:29:44 -0300

    feat: 2.Criar uma matriz de cardápio

--- 1sem_cp3_minicoffee.py
+++ 1sem_cp3_minicoffee.py
@@ -6,3 +6,9 @@ nome = input('Digite seu nome: ')
 qnt_letras = len(nome)
 primeira_letra = nome[0]
 
+cardapio = [
+    [100, 'Expresso', 5.0],
+    [101, 'Café com Leite', 6.0],
+    [102, 'Pão de Queijo', 7.0],
+    [103, 'Refigerante', 10.0],
+]
